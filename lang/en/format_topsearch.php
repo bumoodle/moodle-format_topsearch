1abdb8676caa026492894f648c6dd67e46305227
Kyle Temkin <ktemkin@binghamton.edu>
2012-09-02 19:32:49 -0400

    BU-30 added missing language string

--- lang/en/format_topsearch.php
+++ lang/en/format_topsearch.php
@@ -30,3 +30,4 @@ $string['page-course-view-topics'] = 'Any course main page in topics format';
 $string['page-course-view-topics-x'] = 'Any course page in topics format';
 $string['hidefromothers'] = 'Hide topic';
 $string['showfromothers'] = 'Show topic';
+$string['currentsection'] = 'This topic';
